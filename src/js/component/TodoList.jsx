b078e88104d17a1a11d04de8cdb4ba10fe6e70c4
Jorge Vila <45121848+jorviltim@users.noreply.github.com>
2025-01-27 19:49:05 +0000

    todoList Recomentado jvila

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -5,12 +5,16 @@ const TodoList = () => {
     //estado para manejar el texto de la tarea que se escribe
     const [inputValue, setinputValue] = useState("");
     const [tasks, setTasks] = useState([])//Lista de tareas 
+
+
     const [hoveredTask, setHoveredTask] = useState(null); // Estado para la tarea en hover y poder ocultar boton de borrado
 
     const deleteTask = (indexToDelete) => {//funcion para eliminar la tarea despues de pulsar el boton eliminar
         const filteredTasks = tasks.filter((_, index) => index !== indexToDelete);
         setTasks(filteredTasks); // Actualizamos las tareas
     };
+
+
     return (
         <div>
             {/*CAMPO DE ENTRADA Del Formulario_______________________________*/}
@@ -21,12 +25,12 @@ const TodoList = () => {
                     type="text"
                     className="form-control"
                     placeholder="Escriba una tarea aqui señorit@"
-                    value={inputValue}//aqui viculamos el estado al input 
+                    value={inputValue}//aqui vinculamos el estado al input 
                     onChange={(status) => setinputValue(status.target.value)}//actualizamos el estado al escribir
                     onKeyDown={(status) => {
                         if (status.key === "Enter" && inputValue.trim() !== "") {
-                            setTasks([...tasks, inputValue]);// Añadimos la nueva tarea al array
-                            setinputValue("");//limpiamos el campo de entrada
+                            setTasks([...tasks, inputValue]);// Añadimos la nueva tarea al array de tasks 
+                            setinputValue("");//limpiamos el campo de entrada ya quelo hemos mandado a introducir en el array 
                         }
                     }}
                 />
@@ -40,15 +44,15 @@ const TodoList = () => {
                         No hay tareas, añadir tareas
                     </li>
                 ) : (// en caso de haber añadido una tarea mostararla 
-                    tasks.map((task, index) => (
-                        <li key={index}
+                    tasks.map((task, index) => (//cojo dos parametros 1-lo que hay dentro del array 2 el index del array
+                        <li key={index}//estoy recogiendo la tarea selecionada con el index
                             className="list-group-item d-flex justify-content-between align-items-center"
-                            onMouseEnter={() => setHoveredTask(index)} // Asignamos el índice de la tarea en hover
-                            onMouseLeave={() => setHoveredTask(null)} // Restablecemos el hover
+                            onMouseEnter={() => setHoveredTask(index)} // Asignamos el índice de la tarea en hover cuando señalo la casilla de la lista 
+                            onMouseLeave={() => setHoveredTask(null)} // Restablecemos el hover cuado dejode señalar la casilla de la lista 
                         >
-                            {task}
+                            {task} {/* Muestra la tarea guardada en la lista asignada a ese indice */}
                             {/* Mostrar botón solo si esta tarea está en hover */}
-                            {hoveredTask === index && (
+                            {hoveredTask === index && (//si el index i el boton estan alineados entonces borramos la tarea deese index
                                 <button
                                     className="btn btn-danger btn-sm"
                                     onClick={() => deleteTask(index)} // Lógica para eliminar tarea
